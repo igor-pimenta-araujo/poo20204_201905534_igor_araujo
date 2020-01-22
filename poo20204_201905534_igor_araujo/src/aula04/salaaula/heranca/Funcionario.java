59272740471af9e601b7f56e1e9c6914c94bbc3f
igor-pimenta-araujo <igoraraujo@discente.ufg.br>
2020-01-21 21:53:03 -0300

    Atualizacao aula 4

--- poo20204_201905534_igor_araujo/src/aula04/salaaula/heranca/Funcionario.java
+++ poo20204_201905534_igor_araujo/src/aula04/salaaula/heranca/Funcionario.java
@@ -1,5 +1,48 @@
 package aula04.salaaula.heranca;
 
-public class Funcionario {
+public class Funcionario extends PessoaFisica {
+	int matricula;
+	double salario;
+	String cargo;
 
+	public Funcionario(String nome, String endereco, String telefone, String cpf, String sexo, String estadoCivil,
+			int matricula, double salario, String cargo) {
+		super(nome, endereco, telefone, cpf, sexo, estadoCivil);
+		this.matricula = matricula;
+		this.salario = salario;
+		this.cargo = cargo;
+	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(super.toString());
+		sb.append("Matricula do Funcionario: " + this.getMatricula());
+		sb.append("Salario do Funcionario: " + this.getSalario());
+		sb.append("Cargo do Funcionario: " + this.getCargo());
+		return sb.toString();
+	}
 }
